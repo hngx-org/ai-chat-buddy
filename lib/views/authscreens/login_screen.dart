8ea3c1bf700d0f8bd80b92160d87a076f2133593
Dre_baba <akinyedre007@gmail.com>
2023-10-01 15:07:39 +0100

    changes

--- lib/views/authscreens/login_screen.dart
+++ lib/views/authscreens/login_screen.dart
@@ -27,71 +27,74 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GradientBackground(
-          child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 20.0, right: 20),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 100,
-                ),
-                ChatBuddyText(
-                  largeText: "Chat Buddy",
-                  smallText: "Please Login To Your Account",
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const InfoFilelds(
-                  hintText: 'Enter your email',
-                  icon: Icon(Icons.person),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                InfoFilelds(
-          obscureText: _passwordVisible,
-                  hintText: "Enter your password",
-                  icon: Icon(Icons.lock),
-                  trailing: IconButton(
-                    icon: Icon(_passwordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off),
-                    onPressed: () {
-                      setState(() {
-                        _passwordVisible = !_passwordVisible;
-                      });
-                    },
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 100,
                   ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
+                  ChatBuddyText(
+                    largeText: "Chat Buddy",
+                    smallText: "Please Login To Your Account",
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const InfoFilelds(
+                    hintText: 'Enter your email',
+                    icon: Icon(Icons.person),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  InfoFilelds(
+                    obscureText: _passwordVisible,
+                    hintText: "Enter your password",
+                    icon: Icon(Icons.lock),
+                    trailing: IconButton(
+                      icon: Icon(_passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off),
                       onPressed: () {
-                        Get.to(ForgotPasswordPage());
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
                       },
-                      child: Text(
-                        'Forgot Password?',
-                        style: TextStyle(
-                            fontSize: 15, color: AppColors.buttonColor2),
-                      )),
-                ),
-                const SizedBox(height: 30),
-                const AuthScreenButtons(
-                  onTap: null,
-                  text: 'Login',
-                ),
-                const SizedBox(
-                  height: 100,
-                ),
-                InLineTexts(
-                    firstText: 'Dont have an account?',
-                    secondText: 'Sign Up',
-                    onpressed: null),
-              ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {
+                          Get.to(ForgotPasswordPage());
+                        },
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                              fontSize: 15, color: AppColors.buttonColor2),
+                        )),
+                  ),
+                  const SizedBox(height: 30),
+                  const AuthScreenButtons(
+                    onTap: null,
+                    text: 'Login',
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  InLineTexts(
+                      firstText: 'Dont have an account?',
+                      secondText: 'Sign Up',
+                      onpressed: null),
+                ],
+              ),
             ),
-
-          
+          ),
+        ),
+      ),
+    );
   }
 }
