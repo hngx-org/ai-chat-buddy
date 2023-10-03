9a3b5220dd2da291c5033bdcbb7d534a111fda75
Sparklinglily <carlaejemeh@gmail.com>
2023-10-04 00:47:19 +0100

    database logic

--- lib/database/local_db.dart
+++ lib/database/local_db.dart
@@ -1,6 +1,33 @@
-class LocalDatabase {
+import 'dart:ffi';
 
-  Future addItem<E>({required E item}) async{}
-  Future deleteItem<E>({required E item}) async {}
-  Future updateItem<E>({required E item}) async {}
+import 'package:hive_flutter/hive_flutter.dart';
+
+//generic class for Hive storage
+class OfflineDatabase {
+  final String dbName;
+
+  OfflineDatabase({required this.dbName});
+
+  Future addData<E>(E data) async {
+    await Hive.box<E>(dbName).add(data);
+  }
+
+  Future retrieveData<E>() async {
+    final dataList = await Hive.box<E>(dbName);
+
+    if (dataList.isNotEmpty) {
+      return dataList.values;
+    }
+  }
+
+  Future updateData<E>(E data, int index) async {
+    await Hive.box<E>(dbName).putAt(index, data);
+  }
+
+  Future deleteData<E>(E data) async {
+    await Hive.box<E>(dbName).keys.map((e) async =>
+        await Hive.box<E>(dbName).get(e) == data
+            ? await Hive.box<E>(dbName).delete(e)
+            : print(""));
+  }
 }
